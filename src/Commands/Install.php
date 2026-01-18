362baab2f4448dfbd18be778a7c0677314b3a4ee
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-18 09:51:25 +0100

    Version bump to 1.0.2

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -13,7 +13,7 @@ class Install extends CommandLineInstallSQL  implements ICommandline
     }
     public static $shortName  = 'scss';
     public static $files = [
-        'install/ddl' => 'setup ddl',
+        // 'install/ddl' => 'setup ddl',
         'install/addcommand' => 'setup addcommand',
         'install/scss' => 'setup scss',
         'install/scss.ds' => 'setup scss.ds'
