3b6d42c68c6fdfd10c6542098b106fec4aaf4942
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-19 20:36:48 +0100

    Version bump to 1.0.8

--- src/functions.php
+++ src/functions.php
@@ -7,9 +7,10 @@ require_once "Routes/SetupRoutes.php";
 
 require_once "Compiler.php";
 
+require_once "Commands/InstallMenuSQLCommandline.php";
+require_once "Commands/Import.php";
 require_once "Commands/Install.php";
+
 require_once "Checks/Configuration.php";
 require_once "Checks/Tables.php";
 require_once "Checks/CheckEmpty.php";
-require_once "Commands/InstallMenuSQLCommandline.php";
-require_once "Commands/Import.php";
