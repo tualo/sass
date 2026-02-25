7960a0147e10ecb803f83cb04004cc166d32faba
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-02-25 19:13:09 +0100

    Version bump to 1.0.12

--- src/Sass.php
+++ src/Sass.php
@@ -5,6 +5,7 @@ namespace Tualo\Office\Sass;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\Path;
+use MatthiasMullie\Minify\CSS;
 
 class Sass
 {
@@ -30,6 +31,7 @@ class Sass
             BasicRoute::checkDoubleDots($matches, 'file', 'Path contains ".."') &&
             file_exists(Path::join($dir, $matches['file']))
         ) {
+            self::compile(str_replace('.min', '', str_replace('.css', '', $matches['file'])));
             App::etagFile(Path::join($dir, $matches['file']), true);
             BasicRoute::$finished = true;
             http_response_code(200);
@@ -38,5 +40,55 @@ class Sass
             BasicRoute::$finished = true;
             http_response_code(200);
         }
+    }
+
+
+
+    public static function compile(string $fileName)
+    {
+        if (($cmd = App::configuration('scss', 'cmd', false)) == false) throw new \Exception('scss cmd not found');
+
+        $sql = 'select * from scss order by filename';
+        $db = App::get('session')->getDB();
+        $data = $db->direct($sql);
+        $etag = "";
+        foreach ($data as $row) {
+            $filename = Path::join(Sass::getCompilerPath(), $row['filename']);
+            $dirname = dirname($filename);
+            if (!is_dir($dirname)) {
+                mkdir($dirname, 0777, true);
+            }
+            file_put_contents($filename, $row['content']);
+            $etag = md5($etag . md5($row['content']));
+        }
+
+        $resfilename = Path::join(Sass::getCachePath(), $fileName);
+        if (!is_dir(dirname($resfilename))) {
+            mkdir(dirname($resfilename), 0777, true);
+        }
+
+        if (file_exists(Path::join(Sass::getCompilerPath(), $resfilename . '.etag'))) {
+            $old_etag = file_get_contents(Path::join(Sass::getCompilerPath(), $resfilename . '.etag'));
+            if ($old_etag == $etag && file_exists($resfilename)) {
+                return;
+            }
+        }
+
+        if (!file_exists(Path::join(Sass::getCompilerPath(), $fileName . '.scss')))
+            throw new \Exception('scss entry point not found');
+
+        $entryPoint = Path::join(Sass::getCompilerPath(), $fileName . '.scss');
+        exec($cmd . ' ' . $entryPoint . ' ' . $resfilename . ' 2>&1', $return, $res_code);
+        file_put_contents($resfilename . '.etag', $etag);
+
+        App::result('return', $return);
+        if ($res_code != 0) {
+            App::result('return', $return);
+            throw new \Exception('scss compile error');
+        } else {
+        }
+        $minifier = new CSS($resfilename);
+        $resfilename = Path::join(Sass::getCachePath(), $fileName . '.min.css');
+        $minifier->minify($resfilename);
     }
 }
